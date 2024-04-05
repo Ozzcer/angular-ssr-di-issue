7ab19b4ea9624c0148cf6663cc69298ea81e2993
German Fica <germafica@gmail.com>
2024-04-05 14:08:15 -0300

    Add SSR script and update server-side obj handling
    Added a new script entry in `package.json` to address the Server-Side Rendering (SSR) issue in the Angular project and updated the `AppComponent` to correctly handle server-side objects.
    
    Changes:
    - Add `serve:ssr:ssr-injection-issue` script in `package.json` to run SSR with Express.
    - Update `AppComponent` in `src/app/app.component.ts` to handle server-side objects using `isPlatformServer` and inject `PLATFORM_ID`, `REQUEST`, and `RESPONSE` properly.
    - Use conditional logic to ensure server-side objects are only logged on the server.
    
    Notes:
    - These changes fix the SSR issue by ensuring the Angular app can be rendered on the server side and correctly handles the server-side objects.
    - Remember to run `npm run build` followed by `npm run serve:ssr:ssr-injection-issue` to test the changes.
    - Issue reference: https://github.com/angular/angular-cli/issues/26323

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { APP_BASE_HREF, CommonModule } from '@angular/common';
-import { Component, Inject, Optional } from '@angular/core';
+import { APP_BASE_HREF, CommonModule, isPlatformServer } from '@angular/common';
+import { Component, Inject, Optional, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Response } from 'express';
 import { REQUEST, RESPONSE } from '../express.tokens';
 
 @Component({
@@ -14,12 +15,17 @@ export class AppComponent {
   title = 'ssr-injection-issue';
 
   constructor(
-    @Optional() @Inject(APP_BASE_HREF) private appBaseHref: string,
-    @Optional() @Inject(REQUEST) private request: string,
-    @Optional() @Inject(RESPONSE) private response: string
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Optional() @Inject(APP_BASE_HREF) private appBaseHref: Response,
+    @Optional() @Inject(REQUEST) private request: Response,
+    @Optional() @Inject(RESPONSE) private response: Response
   ) {
-    console.log('basehref', this.appBaseHref);
-    console.log('request', this.request);
-    console.log('response', this.response);
+    if (isPlatformServer(platformId) && this.response) {
+      console.log('basehref', this.appBaseHref);
+      console.log('request', this.request);
+      console.log('response', this.response);
+      // this.response.status(404); // if you want to send a 404 status code error to the client (this won't work if you don't reload the page, as angular handles routes internally in the browser)
+      console.log('This console output will only appear server-side, not client-side (browser).');
+    }
   }
 }
